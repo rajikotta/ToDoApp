31ebec74f704b61944d2ee7f4b70e98e29f24742
raji <raji.chithrangathan@goldenscent.com>
2022-11-05 13:44:15 +0400

    navigation graph and start destination added room db and dao added entity class added fragment layut and recyclerview item layout added

---
+++ app/src/main/java/com/raji/todoapp/data/TaskDatabase.kt
@@ -0,0 +1,11 @@
+package com.raji.todoapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase:RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+}
---
+++ app/src/main/java/com/raji/todoapp/ui/fragment/TaskListFragment.kt
@@ -0,0 +1,7 @@
+package com.raji.todoapp.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.raji.todoapp.R
+
+class TaskListFragment:Fragment(R.layout.fragment_tasks) {
+}
