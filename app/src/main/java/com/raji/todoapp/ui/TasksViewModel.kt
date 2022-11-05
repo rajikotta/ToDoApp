896ee6a632a38b649405aff3d4168dd6b3a84dee
raji <raji.chithrangathan@goldenscent.com>
2022-11-05 15:18:56 +0400

    list adapter added

--- app/src/main/java/com/raji/todoapp/ui/TasksViewModel.kt
+++ app/src/main/java/com/raji/todoapp/ui/TasksViewModel.kt
@@ -1,10 +1,13 @@
 package com.raji.todoapp.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.raji.todoapp.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(private val taskDao: TaskDao) : ViewModel() {
+    val tasks = taskDao.getTasks().asLiveData()
+
 }
--- app/src/main/java/com/raji/todoapp/ui/fragment/TaskListFragment.kt
+++ app/src/main/java/com/raji/todoapp/ui/fragment/TaskListFragment.kt
@@ -1,8 +1,13 @@
 package com.raji.todoapp.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.raji.todoapp.R
+import com.raji.todoapp.databinding.FragmentTasksBinding
+import com.raji.todoapp.ui.TaskListAdapter
 import com.raji.todoapp.ui.TasksViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,4 +15,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class TaskListFragment : Fragment(R.layout.fragment_tasks) {
 
     private val taskListViewModel by viewModels<TasksViewModel>()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TaskListAdapter()
+
+        binding.apply {
+            rvTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        taskListViewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
+
 }
